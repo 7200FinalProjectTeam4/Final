8bf18bac88bcc117e5730800e640d1fcff97275c
GaryZ27 <47007338+GaryZ27@users.noreply.github.com>
2020-12-12 17:01:57 -0500

    Update FileConfig.scala

--- CSYE7200_FINAL_PROJECT-master/src/main/scala/com/edu/neu/csye7200/finalproject/configure/FileConfig.scala
+++ CSYE7200_FINAL_PROJECT-master/src/main/scala/com/edu/neu/csye7200/finalproject/configure/FileConfig.scala
@@ -2,9 +2,8 @@ package csye7200FinalProject.Configure
 
 object FileConfig {
   val dataDir = "Input/"
-  val ratingFile = dataDir + "ratings_small.csv"
-  val bookFile = dataDir + "Book1_100k.csv"
+  val ratingFile = dataDir + "ratings.csv"
+  val bookFile = dataDir + "books.csv"
   val linkFile = dataDir + "links.csv"
   val keywordsFile = dataDir + "keywords.csv"
-  val creditFIle = dataDir + "credits.csv"
 }
