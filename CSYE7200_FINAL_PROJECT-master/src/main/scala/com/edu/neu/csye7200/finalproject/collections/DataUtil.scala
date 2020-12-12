738707062701a6275f6e5cfc7e186b76b0ca1c72
GaryZ27 <47007338+GaryZ27@users.noreply.github.com>
2020-12-12 16:59:15 -0500

    Update DataUtil.scala

--- CSYE7200_FINAL_PROJECT-master/src/main/scala/com/edu/neu/csye7200/finalproject/collections/DataUtil.scala
+++ CSYE7200_FINAL_PROJECT-master/src/main/scala/com/edu/neu/csye7200/finalproject/collections/DataUtil.scala
@@ -14,11 +14,6 @@ object DataUtil {
     .getOrCreate()
   lazy val bookDF=spark.read.option("header", true).schema(BookSchema. bookSchema).csv("Book1_100k.csv")
 
-  /**
-   * Get RDD object from ratings.csv file
-   * @param file   The path of the file
-   * @return       RDD of [[(Long, Rating)]] with( user, product, rating)
-   */
   def getAllRating(file: String) = {
     var rating = spark.read.textFile(file)
     val header = rating.first()
@@ -36,10 +31,7 @@ object DataUtil {
     df.select(colsList: _*).rdd.filter(_(0)!= null).filter(_(1)!=null).map(row => (row.getInt(0), row.getString(1))).filter(x=>x._2.equals(content)).collect
   }
 
-  /**
-   * Get all the movie data of Array type
-   * @return       Array of [[(Int, String)]] contiaining (movieId, title)
-   */
+
   def getBooksArray  = {
     import spark.implicits._
     // There are some null id in movies data and filter them out
@@ -48,13 +40,7 @@ object DataUtil {
 
 
   def getBooksDF = bookDF
-
-  /**
-   * Get the rating information of specific user
-   * @param file   The path of the file
-   * @param userId user Id
-   * @return       RDD of[[Rating]] with (user, product, rating)
-   */
+  
   def getRatingByUser(file: String, userId: Int) = {
     var rating = spark.read.textFile(file)
     val header = rating.first()
@@ -67,11 +53,7 @@ object DataUtil {
       .map(_._2)
   }
 
-  /**
-   * Get the id
-   * @param file   The path of file
-   * @return       Map of [[Int, Int]]
-   */
+
   def getLinkData(file: String) = {
     val df = spark.read.option("header", true).schema(BookSchema.linkdataSchema).csv(file)
     import spark.implicits._
@@ -88,11 +70,6 @@ object DataUtil {
     books.filter(x => links.get(x._1).nonEmpty).map(x => (links(x._1), x._2)).toMap
   }
 
-  /**
-   * Get the keywords of movies which keywords formed in JSON format
-   * @param file   The path of file
-   * @return       DataFrame of keywords
-   */
   def getKeywords(file: String) = {
     spark.read.option("header", true).schema(BookSchema.keywordsSchema).csv(file)
   }
