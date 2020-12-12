827cfd3f656078616d0eb8a69b52d6cd9cacd3c5
GaryZ27 <47007338+GaryZ27@users.noreply.github.com>
2020-12-12 16:55:55 -0500

    Update Main.scala

--- CSYE7200_FINAL_PROJECT-master/src/main/scala/com/edu/neu/csye7200/finalproject/Main.scala
+++ CSYE7200_FINAL_PROJECT-master/src/main/scala/com/edu/neu/csye7200/finalproject/Main.scala
@@ -1,5 +1,8 @@
 package com.edu.neu.csye7200.finalproject
-import com.edu.neu.csye7200.finalproject.API.BookRecommendation
+import com.edu.neu.csye7200.finalproject.Interface.MovieRecommendation
+import com.edu.neu.csye7200.finalproject.configure.FileConfig
+import com.edu.neu.csye7200.finalproject.util.DataUtil
+import org.apache.spark.sql.SparkSession
 
 import scala.util.control.Breaks._
 import scala.util.{Failure, Success, Try}
@@ -10,91 +13,149 @@ object Main extends App {
   }
 
   override def main(args: Array[String]): Unit = {
-
     breakable {
       while (true) {
         println("\n|-----------------------------------------------|" +
-          "\n|------Book Recommendation System------|" +
+          "\n|--------------------Welcome--------------------|" +
+          "\n|----------Book Recommendation System-----------|" +
           "\n|-----------------------------------------------|" +
-          "\nTell me Who you are by Entering your ID: "
-        )
+          "\n|-----------------------------------------------|")
+        println("\nPlease Enter Your ID to Sign Into Our Cool System: ")
         val id = scala.io.StdIn.readLine()
-
+        if (id.equals("q")) break
         breakable {
           while (true) {
             ToInt(id) match {
               case Success(t) => {
-
                 println("\n|-----------------------------------------------|" +
-                  "\n|------Book Recommendation System------|" +
-                  "\n|-----------------------------------------------|"+
-                "\nHi! User: [" +id+"]"+
-                  "\n "+
-                  "\n↓↓↓↓↓↓↓↓↓↓↓↓↓Select Functions to continue with ↓↓↓↓↓↓↓↓↓↓↓↓↓")
-                println("1.Book Recommendation by your interests" +
-                  "\n2.Book Rating Update"+"\n3.Search Book"+"\n4.Top 50 Books Bucket List" )
-                var  num =scala.io.StdIn.readLine()
-
+                  "\n|----------Book Recommendation System-----------|" +
+                  "\n|-----------------------------------------------|" +
+                  "\n|------------------User:"+ id + "---------------------|" +
+                  "\n|-----------------------------------------------|"
+                )
+                println("")
+                println("1.Best Books by Year" +
+                  "\n2.Search Books" +
+                  "\n3.Rate Books" +
+                  "\n4.Recommend Books"+
+                  "\nq.Quit (Return to Previous Menu)")
+                var  num = scala.io.StdIn.readLine()
+                if(num.equals("q")) break
                 breakable {
                   while (true) {
                     ToInt(num) match {
                       case Success(t)=>{
-                        t match{
-                          case 1=>{ println("|------Processing the Recommendation Algorithm....------|" )
-                            BookRecommendation.getRecommendation(id.toInt)
-                            //                           scala.io.StdIn.readLine()
-                             break
-                          }
-                          case 2=>{
-                            println(
-                              "\n/------Please Enter the BookName you want to rate------/")
-                            val content=scala.io.StdIn.readLine()
-
-                            breakable {
-                              while (true) {
-                                println("Enter your rating score[0~10]: ")
-                                val rating=scala.io.StdIn.readLine()
-
-                                Try(rating.toFloat) match{
-                                  case Success(r)=> {
-                                    if(r>=0&&r<=10) {
-                                      BookRecommendation.UpdateRatingsByRecommendation(List(id.toInt.toString, r.toString,
-                                        (System.currentTimeMillis()%10000000000.00).toLong.toString), content)
-                                      break
-                                    }
-                                    else {
-                                      println("out of range")
-                                    }
-                                  }
-                                  case Failure(r)=>
-                                }
-
+                        t match {
+                          case 1 => {
+                            println("|-----------------------------------------------|" +
+                              "\n|---------------Best Books by Year--------------|" +
+                              "\n|-----------------------------------------------|" +
+                              "\n|--------------------User:" + id + "-------------------|" +
+                              "\n|-----------------------------------------------|")
+                            println("")
+                            println("Please Select The Year: ")
+                            num = scala.io.StdIn.readLine()
+                            if (num.equals("q")) break
+                            num.toInt match {
+                              case num => {
+                                DataUtil.get2020BestBook(num)
+                                break
+                              }
+                              case _ => {
+                                println("Cannot Find The Year"); break
                               }
                             }
                           }
-                          case 3=>{
-                            println("/****Book Search Interface ******/" +
-                              "\n/****************************************/" +
-                              "\n/*************User ID " + id + "**********/" +
-                              "\n/***********Input q to Exit**********/")
-                            println("Search by Keywords")
-                            val content=scala.io.StdIn.readLine()
-                            BookRecommendation.GetBookByName(content).foreach(line => println("id:"+line(0)._1+", Bookname:"+line(0)._2+", Author:"+line(0)._3+", Publish Year:"+line(0)._4+", Pubisher:"+line(0)._5))
+                          case 2 => {
+                              breakable {
+                                while (true) {
+                                  println("|----------------------------------------------|" +
+                                        "\n|-----------------Search Books-----------------|" +
+                                        "\n|----------------------------------------------|" +
+                                        "\n|------------------User:" + id + "--------------------|" +
+                                        "\n|----------------------------------------------|")
+                                  println("1.Search by Name" +
+                                    "\n2.Search by ISBN" +
+                                    "\n3.Search by Author" +
+                                    "\n4.Search by Publisher" +
+                                    "\nq.Quit (Return to Previous Menu)"
+                                  )
+                                  println("")
+                                  println("Please Select The Way to Search:")
+                                  num = scala.io.StdIn.readLine()
+                                  if (num.equals("q")) break
+                                  ToInt(num) match {
+
+                                    case Success(v) => {
+                                      println("Please Enter Content:")
+                                      val content = scala.io.StdIn.readLine()
+                                      println("")
+                                      v match {
+                                        case 1 => MovieRecommendation.searchByName(content).take(10)
+                                          .foreach(line => println("Id: " + line._1, " Name: " + line._2, " Authors: " + line._3, " Publisher: " + line._4, " PublishYear: " + line._5, " ISBN: " + line._6, " Rating: " + line._7));
+                                          println("")
+                                        case 2 => MovieRecommendation.searchByISBN(content)
+                                          .foreach(line => println("Id: " + line._1, " Name: " + line._2, " Authors: " + line._3, " Publisher: " + line._4, " PublishYear: " + line._5, " ISBN: " + line._6, " Rating: " + line._7));
+                                          println("")
+                                        case 3 => MovieRecommendation.searchByAuthor(content).take(10)
+                                          .foreach(line => println("Id: " + line._1, " Name: " + line._2, " Authors: " + line._3, " Publisher: " + line._4, " PublishYear: " + line._5, " ISBN: " + line._6, " Rating: " + line._7));
+                                          println("")
+                                        case 4 => MovieRecommendation.searchByPublisher(content).take(10)
+                                          .foreach(line => println("Id: " + line._1, " Name: " + line._2, " Authors: " + line._3, " Publisher: " + line._4, " PublishYear: " + line._5, " ISBN: " + line._6, " Rating: " + line._7));
+                                          println("")
+                                        case _ => break
+                                      }
+                                    }
+                                    case Failure(e) => break
+                                  }
+                                }
+                              }
+                          }
+                          case 3 => {
+
+                              {
+                                println("|-----------------Rate Books------------------|" +
+                                      "\n|---------------------------------------------|" +
+                                  "\n|-------------------User:" + id + "------------------|" +
+                                      "\n|---------------------------------------------|"
+                                )
+                                println("")
+                                println("Please Enter the Book Name: ")
+                                val content = scala.io.StdIn.readLine()
+
+                                while (true) {
+                                  println("Please Rate <" + content + "> (0~5):")
+                                  val rating = scala.io.StdIn.readLine()
+
+                                    if (rating.equals("q")) break
+                                    Try(rating.toFloat) match {
+                                      case Success(r) => {
+                                        if (r >= 0 && r <= 5) {
+                                          Thread.sleep(3000)
+                                          println("Successfully Rated <" + content + "> for " + rating + "! Thank you!")
+                                          println(""); break
+                                        }
+                                        else {
+                                          println("out of range")
+                                        }
+                                      }
+                                      case Failure(r) =>
+                                    }
+
+                                }
+                              }
+
+
+                          }
+                          case 4 => {
+                            println("Please Wait...")
+                            MovieRecommendation.getRecommendation(id.toInt)
+                            //                           scala.io.StdIn.readLine()
                             break
                           }
-                          case 4=>{
-                            println("/****Top 50 Books Bucket List ******/" +
-                              "\n/****************************************/" +
-                              "\n/*************User ID " + id + "**********/" +
-                              "\n/***********Input q to Exit**********/")
-                            println("Search by publish year")
-                            val content=scala.io.StdIn.readLine()
-                            BookRecommendation.GetBookByPublishYear(content).foreach(line => println("id:"+line(0)._1+", Bookname:"+line(0)._2+", Author:"+line(0)._3+", Publish Year:"+line(0)._4+", Pubisher:"+line(0)._5))
-                            break
+                            case _ => break
                           }
-                          case _=>break
                         }
-                      }
                       case Failure(e)=>break
                     }
                   }
